53ce68e60c0cdade76f942707da383a5f57f1ed7
arthurtesta93 <arthurtesta@gmail.com>
2024-04-24 21:26:46 -0300

    initial commit

---
+++ src/main/java/com/trabalhoum/controleassinatura/ControleAssinaturaApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhoum.controleassinatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAssinaturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleAssinaturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trabalhoum/controleassinatura/ControleAssinaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhoum.controleassinatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAssinaturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
