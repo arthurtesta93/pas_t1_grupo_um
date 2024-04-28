2287936671a6946634e510667ca05be8090054f1
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2024-04-28 17:48:37 -0300

    Creating SubscriptionEntity class

--- src/main/java/com/trabalhoum/controleassinatura/domain/entities/SubscriptionEntity.java
+++ src/main/java/com/trabalhoum/controleassinatura/domain/entities/SubscriptionEntity.java
@@ -1,2 +1,28 @@
-package com.trabalhoum.controleassinatura.domain.entities;public class SubscriptionEntity {
+package com.trabalhoum.controleassinatura.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "subscription")
+public class SubscriptionEntity {
+
+    @Id
+    @Column(name = "subscription_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long subscriptionCode;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userCode;
+
+    @Column(name = "app_id", nullable = false)
+    private Long appCode;
+
+    @Column(name = "expiration_date", nullable = false)
+    private Long expirationDate;
+
+
 }
