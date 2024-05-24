10145a1f26b74c1c4e5f7cf76079183ea248ecac
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2024-05-24 16:45:59 -0300

    Creating the upDate method

--- src/main/java/com/trabalhoum/controleassinatura/adapter/interfaceAdapter/controllers/ClientController.java
+++ src/main/java/com/trabalhoum/controleassinatura/adapter/interfaceAdapter/controllers/ClientController.java
@@ -13,11 +13,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -60,5 +56,21 @@ public class ClientController {
     @GetMapping(value = "/client")
     public List<ClientEntity> getAll(){
         return clientService.getAll();
+    }
+
+    /*
+     * NOT WORKING YET!!!
+     */
+    @PatchMapping(value = "/client/{id}")
+    public ResponseEntity<?> upDate(@PathVariable ("id") Long id, @RequestBody ClientDTO clientDTO){
+        ClientDTO clientToUpdate;
+        try{
+            clientToUpdate = modelMapper.map(clientService.upDate(id, clientDTO),AppDTO.class);
+            return new ResponseEntity<AppDTO>(clientToUpdate,HttpStatus.OK);
+        }catch (IllegalAccessException iae){
+            System.out.println("Access Error" + iae.getMessage());
+            iae.printStackTrace();
+            return new ResponseEntity<>("Not able to upDate", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
