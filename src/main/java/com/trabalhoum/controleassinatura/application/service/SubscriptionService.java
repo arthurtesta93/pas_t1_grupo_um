1e8ab87b568536f4477f460f0991a472e0a6bcce
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2024-05-25 11:26:26 -0300

    Creating the save method

--- src/main/java/com/trabalhoum/controleassinatura/application/service/SubscriptionService.java
+++ src/main/java/com/trabalhoum/controleassinatura/application/service/SubscriptionService.java
@@ -1,4 +1,31 @@
 package com.trabalhoum.controleassinatura.application.service;
 
+import com.trabalhoum.controleassinatura.adapter.interfaceAdapter.repository.SubscriptionRepository;
+import com.trabalhoum.controleassinatura.application.dto.SubscriptionDTO;
+import com.trabalhoum.controleassinatura.domain.entities.SubscriptionEntity;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class SubscriptionService {
-}
+
+    private SubscriptionRepository subscriptionRepository;
+    private ModelMapper modelMapper;
+
+    /*
+     * Method Save: Save the SubscriptionEntity passed by parameter in the database.
+     * @return an SubscriptionDTO
+     */
+    public SubscriptionDTO save(SubscriptionEntity subscriptionEntity){
+        for(Long id:subscriptionRepository.findAll(subscriptionEntity.getSubscriptionId())){
+            if(id.equals(subscriptionEntity.getSubscriptionId())) {
+                return modelMapper.map(subscriptionRepository.save(subscriptionEntity), SubscriptionDTO.class);
+            }
+        }
+        throw new IllegalArgumentException("Subscription not found");
+        }
+    }
+
+
--- src/main/java/com/trabalhoum/controleassinatura/domain/entities/SubscriptionEntity.java
+++ src/main/java/com/trabalhoum/controleassinatura/domain/entities/SubscriptionEntity.java
@@ -2,10 +2,13 @@ package com.trabalhoum.controleassinatura.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
-
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
